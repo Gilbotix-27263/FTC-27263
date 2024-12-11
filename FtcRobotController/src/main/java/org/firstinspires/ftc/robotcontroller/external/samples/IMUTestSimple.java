c1378ebc1dd2470d4c25e76ea2215a17241397bb
TomerDev <tomer.itzkovitch@gmail.com>
2024-12-11 19:12:16 +0200

    Update IMUTestSimple.java
    gg

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/IMUTestSimple.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/IMUTestSimple.java
@@ -1,37 +1,45 @@
 package org.firstinspires.ftc.robotcontroller.external.samples;
 
-import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.BHI260IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.robotcore.external.navigation.AngularVelocity;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 @TeleOp
-public class IMUTestSimple extends LinearOpMode {
-    private BNO055IMU imu;
+public class BHI260APIMUTest extends LinearOpMode {
+    private BHI260IMU imu; // BHI260AP IMU
 
     @Override
     public void runOpMode() {
         telemetry.addData("Status", "Initializing IMU...");
         telemetry.update();
 
-        imu = hardwareMap.get(BNO055IMU.class, "imui");
+        // Initialize the IMU
+        imu = hardwareMap.get(BHI260IMU.class, "imu"); // Match the name in your hardware configuration
 
-        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
-        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-
-        imu.initialize(parameters);
-
-        if (imu.isGyroCalibrated()) {
-            telemetry.addData("IMU Status", "Calibrated");
-        } else {
-            telemetry.addData("IMU Status", "Not Calibrated");
-        }
+        // Wait for the game to start
+        telemetry.addData("IMU Status", "Initialized");
         telemetry.update();
-
         waitForStart();
 
         while (opModeIsActive()) {
-            telemetry.addData("IMU Yaw", "%.2f", imu.getAngularOrientation().firstAngle);
+            // Retrieve angular velocity (rate of change of orientation)
+            AngularVelocity angularVelocity = imu.getAngularVelocity();
+
+            // Retrieve orientation angles (yaw, pitch, roll)
+            Orientation orientation = imu.getRobotOrientation(BHI260IMU.AxisReference.INTRINSIC, BHI260IMU.AxisOrder.ZYX);
+
+            // Telemetry for angular velocity
+            telemetry.addData("Angular Velocity - Yaw (deg/s)", "%.2f", angularVelocity.zRotationRate);
+            telemetry.addData("Angular Velocity - Pitch (deg/s)", "%.2f", angularVelocity.xRotationRate);
+            telemetry.addData("Angular Velocity - Roll (deg/s)", "%.2f", angularVelocity.yRotationRate);
+
+            // Telemetry for orientation
+            telemetry.addData("Orientation - Yaw (deg)", "%.2f", orientation.firstAngle);
+            telemetry.addData("Orientation - Pitch (deg)", "%.2f", orientation.secondAngle);
+            telemetry.addData("Orientation - Roll (deg)", "%.2f", orientation.thirdAngle);
+
             telemetry.update();
         }
     }
