69daa5329442f21c4bf2d79e6efe237bf06f4478
TomerDev <tomer.itzkovitch@gmail.com>
2024-12-13 02:27:47 +0200

    Update FTCFirstProgram.java

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/FTCFirstProgram.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/FTCFirstProgram.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.robotcontroller.external.samples;
 
-import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
