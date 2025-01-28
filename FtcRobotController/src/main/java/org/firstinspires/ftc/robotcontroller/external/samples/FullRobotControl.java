aa00a70a3e28e98b1ada93c4b96018902500d42d
ElladTheFunnyDog <az4974@eh.amalnet.k12.il>
2025-01-28 11:31:31 +0200

    Update FullRobotControl.java

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/FullRobotControl.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/FullRobotControl.java
@@ -31,7 +31,7 @@ public class FullRobotControl extends LinearOpMode {
 
     // Maximum power for arm motors
     private static final double MAX_ARMUD_POWER = 0.4;
-    private static final double MAX_ARMEX_POWER = 1;
+    private static final double MAX_ARMEX_POWER = 0.8;
 
     private static final double ZERO_DELAY = 1.0; // Minimum delay in seconds between zeroing actions
 
@@ -156,11 +156,11 @@ public class FullRobotControl extends LinearOpMode {
                 int armExCurrentPosition = armEx.getCurrentPosition();
 
                 // Prevent movement when arm is within the range -12 to 0
-                if (armExCurrentPosition >= -50 && gamepad2.right_trigger > 0.1) {
+                if (armExCurrentPosition >= -100 && armExPower > 0) {
                     armEx.setPower(0.0); // Ignore RT input within restricted range
                 }
                 // Prevent backward movement beyond the lower limit of -2150
-                else if (armExCurrentPosition <= -2150 && armExPower < 0) {
+                else if (armExCurrentPosition <= -2250 && armExPower < 0) {
                     armEx.setPower(0.0); // Stop backward movement if beyond limit
                 }
                 // Allow movement within valid range
