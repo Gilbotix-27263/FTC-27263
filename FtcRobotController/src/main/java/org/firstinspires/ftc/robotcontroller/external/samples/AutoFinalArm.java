3ee53d946d8dd1eb1e55ad9d709bad4a3256a9ce
romtzuk355 <rg2631@eh.amalnet.k12.il>
2025-01-29 14:37:45 +0200

    Update AutoFinalArm.java

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/AutoFinalArm.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/AutoFinalArm.java
@@ -1,4 +1,37 @@
 package org.firstinspires.ftc.robotcontroller.external.samples;
 
-public class AutoFinalArm {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+@Autonomous(group = "Test Stuff IDK", name = "Autonomous Arm Test")
+public class AutoFinalArm extends LinearOpMode {
+    private TouchSensor armZeroSensor;
+    private DcMotor armUD, armEx;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        armZeroSensor = hardwareMap.get(TouchSensor.class, "armExZeroSensor");
+        armUD = hardwareMap.get(DcMotor.class, "armUD");
+        armEx = hardwareMap.get(DcMotor.class, "arm");
+
+        waitForStart();
+
+        while(opModeIsActive())
+        {
+
+        }
+    }
+
+    private void moveUD()
+    {
+
+    }
+
+    private void moxeEx()
+    {
+        
+    }
 }
