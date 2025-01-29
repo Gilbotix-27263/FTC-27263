748eeef35488e7fb018fe4cee5affbcaf289a707
romtzuk355 <rg2631@eh.amalnet.k12.il>
2025-01-29 14:48:39 +0200

    Update AutoFinalArm.java

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/AutoFinalArm.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/AutoFinalArm.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.robotcontroller.external.samples;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 //import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 
@@ -10,12 +11,23 @@ import com.qualcomm.robotcore.hardware.TouchSensor;
 public class AutoFinalArm extends LinearOpMode {
     private TouchSensor armZeroSensor;
     private DcMotor armUD, armEx;
+    private CRServo servoIntakeLeft, servoIntakeRight;
 
     @Override
     public void runOpMode() throws InterruptedException {
+        //Me When Copy-Paste LOL
         armZeroSensor = hardwareMap.get(TouchSensor.class, "armExZeroSensor");
         armUD = hardwareMap.get(DcMotor.class, "armUD");
         armEx = hardwareMap.get(DcMotor.class, "arm");
+        servoIntakeLeft = hardwareMap.get(CRServo.class, "intakeLeft");
+        servoIntakeRight = hardwareMap.get(CRServo.class, "intakeRight");
+
+        armUD.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armUD.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        armUD.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE); // Enable brake mode for armUD
+
+        armEx.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armEx.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         waitForStart();
 
@@ -27,11 +39,21 @@ public class AutoFinalArm extends LinearOpMode {
 
     private void moveUD()
     {
+        telemetry.addData("Speed Mode");
+    }
+
+    private void moveEx()
+    {
+        
+    }
+
+    private void pickUp()
+    {
 
     }
 
-    private void moxeEx()
+    private void release()
     {
-        
+
     }
 }
