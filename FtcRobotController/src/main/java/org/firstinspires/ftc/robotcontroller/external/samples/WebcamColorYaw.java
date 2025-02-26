90bca7531231d0c53f28d2fdae89fdb4b21b8a12
TomerDev <tomer.itzkovitch@gmail.com>
2025-02-26 11:01:35 +0200

    Update WebcamColorYaw.java

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/WebcamColorYaw.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/WebcamColorYaw.java
@@ -3,13 +3,14 @@ package org.firstinspires.ftc.robotcontroller.external.samples;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.IMU;
-
+import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvPipeline;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.opencv.core.*;
 import org.opencv.imgproc.Imgproc;
@@ -18,12 +19,16 @@ import org.opencv.imgproc.Imgproc;
 public class WebcamColorYaw extends LinearOpMode {
     private OpenCvCamera webcam;
     private IMU imu;
+    private Servo intake;
+    private Servo intakelr;
     private ColorDetectionPipeline colorPipeline;
     private static final double CAMERA_FOV_DEGREES = 60.0;
 
     @Override
     public void runOpMode() {
         imu = hardwareMap.get(IMU.class, "imu");
+        intake = hardwareMap.get(Servo.class, "intake");
+        intakelr = hardwareMap.get(Servo.class, "intakelr");
 
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
                 "cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
@@ -64,6 +69,17 @@ public class WebcamColorYaw extends LinearOpMode {
             telemetry.addData("Sample Yaw", "%.2f degrees", sampleYaw);
             telemetry.addData("Object Orientation", orientationType);
             telemetry.update();
+
+            if (Math.abs(sampleYawOffset) < 5) { // Sample is centered
+                if ("Vertical".equals(orientationType)) {
+                    intake.setPosition(1.0); // Close intake
+                } else {
+                    intakelr.setPosition(1.0); // Rotate until vertical
+                    sleep(500); // Allow time for rotation
+                    intakelr.setPosition(0.5); // Stop rotation
+                    intake.setPosition(1.0); // Close intake
+                }
+            }
         }
     }
 
@@ -99,12 +115,7 @@ public class WebcamColorYaw extends LinearOpMode {
                 double centerX = boundingRect.x + (boundingRect.width / 2.0);
                 double normalizedX = (centerX - (FRAME_WIDTH / 2.0)) / (FRAME_WIDTH / 2.0);
                 sampleYawOffset = normalizedX * (CAMERA_FOV_DEGREES / 2.0);
-
-                if (boundingRect.width > boundingRect.height) {
-                    orientationType = "Horizontal";
-                } else {
-                    orientationType = "Vertical";
-                }
+                orientationType = boundingRect.width > boundingRect.height ? "Horizontal" : "Vertical";
             }
 
             mask.release();
